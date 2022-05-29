67980558bf1ad196b6f3b933bb04a574b2fc3f8c
nkilm <nikhilmohitelhs@gmail.com>
2022-05-30 02:29:59 +0530

    prevent raw string from getting printed

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
+'use strict';
+
 const express = require("express");
 const router = express.Router();
-
 const data = require("../data/riddles.json")
 
+
 router.get("/", (req, res) => {
-    res.send(data[Math.floor(Math.random()*data.length)])
+    let riddleObj = data[Math.floor(Math.random() * data.length)];
+    res.send(riddleObj);
 })
 module.exports = router;
